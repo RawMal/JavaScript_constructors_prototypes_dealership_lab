9fc5c40d3d3222f250d44b9cdf962b69676980f6
Rawan Malek <rawanmalek@hotmail.co.uk>
2022-09-25 22:41:10 +0100

    count how many cars in stock test

--- dealership.js
+++ dealership.js
@@ -1,16 +1,21 @@
-const { Car } = require("./car");
+
 
 const Dealership = function(name, carCapacity, carsInStock) {
     let _name = name;
     let _carCapacity = carCapacity;
     let _carsInStock = [];
 
-    this.getName = () => _name;
-    this.setName = (name) => _name = name;
-    this.getCarCapacity = () => _carCapacity;
-    this.setCarCapacity = (carCapacity) => _carCapacity = carCapacity;
-    Car.prototype.getcarsInStock = () => _carsInStock;
-    Car.prototype.addCar = (car)=> _cars.push(car);
-    Car.prototype.removeCar = (car)=> _cars.splice(_cars.indexOf(car), 1);
+    // this.getName = () => _name;
+    // this.setName = (name) => _name = name;
+    // this.getCarCapacity = () => _carCapacity;
+    // this.setCarCapacity = (carCapacity) => _carCapacity = carCapacity;
+    // Car.prototype.getcarsInStock = () => _carsInStock;
+    // Car.prototype.addCar = (car)=> _cars.push(car);
+    // Car.prototype.removeCar = (car)=> _cars.splice(_cars.indexOf(car), 1);
+
+    Dealership.prototype.count = function () {
+        return this._carsInStock.length; // Shows how many cars in stock at dealership
+    }
 }
 
+module.exports = {Dealership};
---
+++ dealership.test.js
@@ -0,0 +1,19 @@
+const { name } = require("ci-info");
+const { Car } = require("./car");
+const { Dealership} = require("./dealership");
+
+test("Dealership - count()", () => {
+    let _name = "Test";
+    let _carCapacity = 12;
+    let _carsInStock = [new Car('Ferrari', 250000, '5l V12'), new Car('Maserati', 100000, '5l V8')];
+
+
+    let dealership = new Dealership(_name, _carCapacity, _carsInStock);
+    dealership.cars = cars;
+
+    expect(dealership.count()).toBe(2);
+}
+
+
+
+)
